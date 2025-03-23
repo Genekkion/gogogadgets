487f88ab2146ccf34406824b5c5b80ef2c99b0c1
swoh <genekkion@live.com>
2025-03-23 16:13:19 +0800

    renamed func in set

--- pkg/dsa/set/unordered/config.go
+++ pkg/dsa/set/unordered/config.go
@@ -2,7 +2,7 @@ package set
 
 type SetOption[T comparable] func(*Set[T])
 
-func OptionFromSlice[T comparable](slice []T) SetOption[T] {
+func WithSlice[T comparable](slice []T) SetOption[T] {
 	return func(s *Set[T]) {
 		for _, v := range slice {
 			s.m[v] = emptyValue
