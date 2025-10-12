1dfea72937b8228a70206cb0d2cebc57be957128
The Operator <115409114+Genekkion@users.noreply.github.com>
2025-10-12 17:48:56 +0800

    feat: Update linked-list package (#9)

--- pkg/dsa/linkedlist/list.go
+++ pkg/dsa/linkedlist/list.go
@@ -1,18 +1,17 @@
 package list
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/Genekkion/gogogadgets/pkg/dsa/node"
 )
 
+// LinkedList is a generic doubly-linked list.
 type LinkedList[T any] struct {
 	head *node.LinearNode[T]
 	tail *node.LinearNode[T]
 	size int
 }
 
+// New creates a new linked list.
 func New[T any]() LinkedList[T] {
 	return LinkedList[T]{
 		head: nil,
@@ -21,26 +20,43 @@ func New[T any]() LinkedList[T] {
 	}
 }
 
+// FromExisting creates a new linked list from an existing node.
 func FromExisting[T any](head *node.LinearNode[T]) LinkedList[T] {
-
-	ptr := head
-	count := 0
-	for ptr != nil {
-		count++
-		ptr = ptr.GetNext()
-	}
-
-	return LinkedList[T]{
+	l := LinkedList[T]{
 		head: head,
-		tail: ptr,
-		size: count,
 	}
+
+	l.size, l.tail = l.countLength()
+
+	return l
 }
 
+// countLength returns the length of the linked list and the last node.
+func (l LinkedList[T]) countLength() (int, *node.LinearNode[T]) {
+	if l.head == nil {
+		return 0, nil
+	}
+
+	ptr := l.head
+	count := 1
+	for ptr.GetNext() != nil {
+		ptr = ptr.GetNext()
+		count++
+	}
+	return count, ptr
+}
+
+// Len returns the length of the linked list.
 func (l LinkedList[T]) Len() int {
 	return l.size
 }
 
+// IsEmpty returns true if the linked list is empty.
+func (l LinkedList[T]) IsEmpty() bool {
+	return l.Len() == 0
+}
+
+// PopFront removes the first node from the linked list and returns it.
 func (l *LinkedList[T]) PopFront() *node.LinearNode[T] {
 	if l.Len() == 0 {
 		return nil
@@ -50,6 +66,7 @@ func (l *LinkedList[T]) PopFront() *node.LinearNode[T] {
 
 	l.head = l.head.GetNext()
 	l.size--
+
 	if l.size == 0 {
 		l.tail = nil
 	} else {
@@ -58,6 +75,7 @@ func (l *LinkedList[T]) PopFront() *node.LinearNode[T] {
 	return n
 }
 
+// PopBack removes the last node from the linked list and returns it.
 func (l *LinkedList[T]) PopBack() *node.LinearNode[T] {
 	if l.Len() == 0 {
 		return nil
@@ -67,6 +85,7 @@ func (l *LinkedList[T]) PopBack() *node.LinearNode[T] {
 
 	l.tail = l.tail.GetPrev()
 	l.size--
+
 	if l.size == 0 {
 		l.head = nil
 	} else {
@@ -75,35 +94,32 @@ func (l *LinkedList[T]) PopBack() *node.LinearNode[T] {
 	return n
 }
 
+// PushBack adds a node to the end of the linked list.
 func (l *LinkedList[T]) PushBack(n *node.LinearNode[T]) {
+	if l.IsEmpty() {
+		l.head = n
+		l.tail = n
+		l.size = 1
+		return
+	}
+
 	l.tail.SetNext(n)
 	n.SetPrev(l.tail)
 	l.tail = n
 	l.size++
 }
 
+// PushFront adds a node to the beginning of the linked list.
 func (l *LinkedList[T]) PushFront(n *node.LinearNode[T]) {
+	if l.IsEmpty() {
+		l.head = n
+		l.tail = n
+		l.size = 1
+		return
+	}
+
 	l.head.SetPrev(n)
 	n.SetNext(l.head)
 	l.head = n
 	l.size++
-}
-
-func (l LinkedList[T]) String() string {
-	b := strings.Builder{}
-	b.WriteRune('[')
-	ptr := l.head
-	if ptr != nil {
-		b.WriteString(fmt.Sprint(ptr.GetValue()))
-		ptr = ptr.GetNext()
-
-		for ptr != nil {
-			b.WriteString(", ")
-			b.WriteString(fmt.Sprint(ptr.GetValue()))
-			ptr = ptr.GetNext()
-		}
-	}
-	b.WriteRune(']')
-
-	return b.String()
 }
