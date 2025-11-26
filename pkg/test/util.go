a12a43c4705821e133ede8c72f1dc96d67115bda
The Operator <115409114+Genekkion@users.noreply.github.com>
2025-11-26 17:22:55 +0800

    update test utils (#11)

--- pkg/test/util.go
+++ pkg/test/util.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"encoding/json"
 	"fmt"
 	"path/filepath"
 	"reflect"
@@ -8,9 +9,13 @@ import (
 	"testing"
 )
 
-// Prints the caller of the function for debugging purposes.
-func printCaller() {
-	pc, file, line, ok := runtime.Caller(2)
+// printCaller prints the caller of the function for debugging purposes.
+func printCaller(jumps ...int) {
+	jump := 2
+	if len(jumps) > 0 {
+		jump += jumps[0]
+	}
+	pc, file, line, ok := runtime.Caller(jump)
 	if ok {
 		fn := runtime.FuncForPC(pc)
 		fmt.Printf("Called from %s:%d (%s)\n",
@@ -18,52 +23,52 @@ func printCaller() {
 	}
 }
 
-// Assert Checks if a condition is true and fails if not.
-func Assert(t *testing.T, message string, got bool) {
+// Assert prints the error message out in a standard format.
+func Assert(t *testing.T, message string, got bool, jumps ...int) {
+	t.Helper()
 	if got {
 		return
 	}
 
 	t.Errorf("%s", message)
-	fmt.Printf("Expression is false")
+	fmt.Printf("Expression is false\n")
 
-	printCaller()
+	printCaller(jumps...)
 	t.FailNow()
 }
 
-// AssertEqual Checks if two values are equal using reflect.DeepEqual and fails if not.
-func AssertEqual[T any](t *testing.T, message string, expected T, got T) {
+// AssertEqual checks if two values are equal using reflect.DeepEqual and fails if not.
+func AssertEqual[T any](t *testing.T, message string, expected T, got T, jumps ...int) {
+	t.Helper()
 	if !reflect.DeepEqual(expected, got) {
 		t.Errorf("%s", message)
 		fmt.Printf("Expected: %v\n", expected)
 		fmt.Printf("Got     : %v\n", got)
 
-		printCaller()
+		printCaller(jumps...)
 		t.FailNow()
 	}
 }
 
-// NilErr Checks if err is nil, and if not, fails the test.
+// AssertJsonEqual compares two JSON objects and fails the test if they are not equal.
+func AssertJsonEqual(t *testing.T, message string, expected any, got any) {
+	t.Helper()
+	b1, err := json.Marshal(expected)
+	NilErr(t, err)
+	b2, err := json.Marshal(got)
+	NilErr(t, err)
+	AssertEqual(t, message, string(b1), string(b2))
+}
+
+// NilErr checks if err is nil, and if not, fails the test.
 func NilErr(t *testing.T, err error) {
+	t.Helper()
 	if err == nil {
 		return
 	}
 
 	t.Errorf("Expected nil error")
 	fmt.Printf("Error: %v\n", err)
-
-	printCaller()
-	t.FailNow()
-}
-
-// NonNil Checks if the value is non-nil, and if not, fails the test.
-func NonNil[T any](t *testing.T, value *T) {
-	if value != nil {
-		return
-	}
-
-	t.Errorf("Expected non-nil value")
-	fmt.Printf("Value: %v\n", value)
 
 	printCaller()
 	t.FailNow()
